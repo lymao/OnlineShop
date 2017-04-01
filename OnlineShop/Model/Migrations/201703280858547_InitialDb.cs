7aa2143a3fe6ddbb356afdedd610de53a79e7953
DESKTOP-02CNGUS\lymao <lymaodt@gmail.com>
2017-04-01 21:51:01 +0700

    Use session and login

---
+++ OnlineShop/Model/Migrations/201703280858547_InitialDb.cs
@@ -0,0 +1,270 @@
+namespace Model.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialDb : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.About",
+                c => new
+                    {
+                        ID = c.Long(nullable: false, identity: true),
+                        Name = c.String(maxLength: 250),
+                        MetaTitle = c.String(maxLength: 250, unicode: false),
+                        Description = c.String(maxLength: 500),
+                        Image = c.String(maxLength: 250),
+                        Detail = c.String(storeType: "ntext"),
+                        CreateDate = c.DateTime(),
+                        CreateBy = c.String(maxLength: 50, unicode: false),
+                        ModifiedDate = c.DateTime(),
+                        ModifiedBy = c.String(maxLength: 50, unicode: false),
+                        MetaKeywords = c.String(maxLength: 250),
+                        MetaDescriptions = c.String(maxLength: 250),
+                        Status = c.Boolean(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Category",
+                c => new
+                    {
+                        ID = c.Long(nullable: false, identity: true),
+                        Name = c.String(maxLength: 250),
+                        MetaTitle = c.String(maxLength: 250, unicode: false),
+                        ParentID = c.Long(),
+                        DisplayOrder = c.Int(),
+                        SeoTitle = c.String(maxLength: 250),
+                        CreateDate = c.DateTime(),
+                        CreateBy = c.String(maxLength: 50, unicode: false),
+                        ModifiedDate = c.DateTime(),
+                        ModifiedBy = c.String(maxLength: 50, unicode: false),
+                        MetaKeywords = c.String(maxLength: 250),
+                        MetaDescriptions = c.String(maxLength: 250),
+                        Status = c.Boolean(),
+                        ShowOnHome = c.Boolean(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Contact",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Content = c.String(storeType: "ntext"),
+                        Status = c.Boolean(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Content",
+                c => new
+                    {
+                        ID = c.Long(nullable: false, identity: true),
+                        Name = c.String(maxLength: 250),
+                        MetaTitle = c.String(maxLength: 250, unicode: false),
+                        Description = c.String(maxLength: 500),
+                        Image = c.String(maxLength: 250),
+                        CategoryID = c.Long(),
+                        Detail = c.String(storeType: "ntext"),
+                        CreateDate = c.DateTime(),
+                        CreateBy = c.String(maxLength: 50, unicode: false),
+                        ModifiedDate = c.DateTime(),
+                        ModifiedBy = c.String(maxLength: 50, unicode: false),
+                        MetaKeywords = c.String(maxLength: 250),
+                        MetaDescriptions = c.String(maxLength: 250),
+                        Status = c.Boolean(),
+                        TopHot = c.DateTime(),
+                        ViewCount = c.Int(),
+                        Tag = c.String(maxLength: 500),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.ContentTag",
+                c => new
+                    {
+                        ContentID = c.Long(nullable: false),
+                        TagID = c.String(nullable: false, maxLength: 50, unicode: false),
+                    })
+                .PrimaryKey(t => new { t.ContentID, t.TagID });
+            
+            CreateTable(
+                "dbo.Feedback",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 50),
+                        Phone = c.String(maxLength: 50),
+                        Email = c.String(maxLength: 50),
+                        Address = c.String(maxLength: 50),
+                        Content = c.String(maxLength: 500),
+                        CreateDate = c.DateTime(),
+                        Status = c.Boolean(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Footer",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 50, unicode: false),
+                        Content = c.String(storeType: "ntext"),
+                        Status = c.Boolean(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Menu",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Text = c.String(maxLength: 50),
+                        Link = c.String(maxLength: 250),
+                        DisplayOrder = c.Int(),
+                        Target = c.String(maxLength: 50),
+                        Status = c.Boolean(),
+                        TypeID = c.Int(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.MenuType",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 50),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.ProductCategory",
+                c => new
+                    {
+                        ID = c.Long(nullable: false, identity: true),
+                        Name = c.String(maxLength: 250),
+                        MetaTitle = c.String(maxLength: 250, unicode: false),
+                        ParentID = c.Long(),
+                        DisplayOrder = c.Int(),
+                        SeoTitle = c.String(maxLength: 250),
+                        CreateDate = c.DateTime(),
+                        CreateBy = c.String(maxLength: 50, unicode: false),
+                        ModifiedDate = c.DateTime(),
+                        ModifiedBy = c.String(maxLength: 50, unicode: false),
+                        MetaKeywords = c.String(maxLength: 250),
+                        MetaDescriptions = c.String(maxLength: 250),
+                        Status = c.Boolean(),
+                        ShowOnHome = c.Boolean(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Product",
+                c => new
+                    {
+                        ID = c.Long(nullable: false, identity: true),
+                        Name = c.String(maxLength: 250),
+                        Code = c.String(maxLength: 10, unicode: false),
+                        MetaTitle = c.String(maxLength: 250, unicode: false),
+                        Description = c.String(maxLength: 500),
+                        Image = c.String(maxLength: 250),
+                        MoreImages = c.String(storeType: "xml"),
+                        Price = c.Decimal(precision: 18, scale: 0),
+                        PromotionPrice = c.Decimal(precision: 18, scale: 0),
+                        IncludedVAT = c.Boolean(),
+                        Quantity = c.Int(),
+                        CategoryID = c.Long(),
+                        Detail = c.String(storeType: "ntext"),
+                        Warranty = c.Int(),
+                        CreateDate = c.DateTime(),
+                        CreateBy = c.String(maxLength: 50, unicode: false),
+                        ModifiedDate = c.DateTime(),
+                        ModifiedBy = c.String(maxLength: 50, unicode: false),
+                        MetaKeywords = c.String(maxLength: 250),
+                        MetaDescriptions = c.String(maxLength: 250),
+                        Status = c.Boolean(),
+                        TopHot = c.DateTime(),
+                        ViewCount = c.Int(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Slide",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Image = c.String(maxLength: 250),
+                        DisplayOrder = c.Int(),
+                        Link = c.String(maxLength: 250),
+                        CreateBy = c.String(maxLength: 50, unicode: false),
+                        CreateDate = c.DateTime(),
+                        ModifiedDate = c.DateTime(),
+                        ModifiedBy = c.String(maxLength: 50, unicode: false),
+                        Status = c.Boolean(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.SystemConfig",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 50, unicode: false),
+                        Name = c.String(maxLength: 50),
+                        Type = c.String(maxLength: 50, unicode: false),
+                        Value = c.String(maxLength: 250),
+                        Status = c.Boolean(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Tag",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 50, unicode: false),
+                        Name = c.String(maxLength: 50),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.User",
+                c => new
+                    {
+                        ID = c.Long(nullable: false, identity: true),
+                        UserName = c.String(maxLength: 50, unicode: false),
+                        Password = c.String(maxLength: 32, unicode: false),
+                        Name = c.String(maxLength: 50),
+                        Address = c.String(maxLength: 50),
+                        Email = c.String(maxLength: 50),
+                        Phone = c.String(maxLength: 50),
+                        CreateDate = c.DateTime(),
+                        CreateBy = c.String(maxLength: 50, unicode: false),
+                        ModifiedDate = c.DateTime(),
+                        ModifiedBy = c.String(maxLength: 50, unicode: false),
+                        Status = c.Boolean(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.User");
+            DropTable("dbo.Tag");
+            DropTable("dbo.SystemConfig");
+            DropTable("dbo.Slide");
+            DropTable("dbo.Product");
+            DropTable("dbo.ProductCategory");
+            DropTable("dbo.MenuType");
+            DropTable("dbo.Menu");
+            DropTable("dbo.Footer");
+            DropTable("dbo.Feedback");
+            DropTable("dbo.ContentTag");
+            DropTable("dbo.Content");
+            DropTable("dbo.Contact");
+            DropTable("dbo.Category");
+            DropTable("dbo.About");
+        }
+    }
+}
---
+++ OnlineShop/OnlineShop/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using Model.Dao;
+using OnlineShop.Areas.Admin.Models;
+using OnlineShop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+                var res = dao.Login(model.UserName, model.Password);
+                if (res)
+                {
+                    var user = dao.GetById(model.UserName);
+                    var userSession = new UserLogin();
+                    userSession.UserName = user.UserName;
+                    userSession.UserId = user.ID;
+                    Session.Add(CommonConstants.USER_SESSION,userSession);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Đăng nhập không đúng.");
+                }
+            }
+            return View("Index");
+        }
+    }
+}
